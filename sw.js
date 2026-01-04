8e868bb19573e2d733321ea660ee75db0c1aba2a
Mohamed-nazih <nazihmohammed335@gmail.com>
2026-01-04 09:26:02 +0530

    Add custom logo to app header and PWA manifest

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'pulsecount-v1';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
+    './logo.png',
     'https://fonts.googleapis.com/css2?family=Outfit:wght@300;500;800&family=Rajdhani:wght@500;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
